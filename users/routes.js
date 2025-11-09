1d848c67ad54a158b8f25c2b539d2b4162882e9b
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2025-11-09 14:18:39 -0600

    Create routes.js

---
+++ users/routes.js
@@ -0,0 +1,13 @@
+// routes/routers.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+module.exports = router;
