3953f3f824609df8d30f5ac7032780636d7815e3
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2025-11-11 15:59:44 -0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,27 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+//The first and last name parameters should be encoded in a 
+//query string e.g. ?first=firstname&last=lastname.
+
+router.post('/name', (req, res, next) => {
+  let firstName = req.body.first;
+  let lastName = req.body.last;
+  res.json({name: `${firstName} ${lastName}`})  
+});
+
+router.get('/nameGet', (req, res, next) => {
+  let firstName = req.query.first;
+  let lastName = req.query.last;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+
+
+//The first and last name parameters should be encoded in a 
+//query string e.g. ?first=firstname&last=lastname.
+
 
 
 module.exports = router;
