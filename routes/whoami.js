2b1bf3952963042e78844dfd888e61cc30622c2a
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2025-11-11 19:49:37 -0600

    Update whoami.js

--- routes/whoami.js
+++ routes/whoami.js
@@ -1,26 +1,14 @@
 // routes/whoami.js
 const express = require('express');
 const router = express.Router();
-
-router.get('/api/whoami', function (req, res) {
-  const ipAddress = IP.address();
-  let localLanguage = req.headers["accept-language"]
-  let newUserAgent = new UserAgent(); 
-  res.json({ ipaddress: ipAddress, language: localLanguage,  software: newUserAgent.toString()});
-});
+const os = require('os');
 
 
-/*
-//get route show ip address
-router.get('/api/ipaddress', (req, res) => {
-    const ipAddress = IP.address();
-    res.send(ipAddress)
-})
-*/
 
 router.get('/api/os', (req, res) => {
-  console.log(os.type());
-  res.send(os.type());
+  const osType = os.type();
+  console.log(osType);
+  res.send(osType);
 });
 
 
