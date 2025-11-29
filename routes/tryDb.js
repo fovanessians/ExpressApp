d1974a22278e893b251a563b207a472f650b127e
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2025-11-29 16:42:39 -0600

    Create tryDb.js

---
+++ routes/tryDb.js
@@ -0,0 +1,14 @@
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+
+router.get("/is-mongoose-ok", function (req, res) {
+  if (mongoose) {
+    res.json({ isMongooseOk: !!mongoose.connection.readyState });
+  } else {
+    res.json({ isMongooseOk: false });
+  }
+});
