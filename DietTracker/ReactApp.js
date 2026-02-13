d8581f3dd0dc29549cf7d587d45b8f8c0d74c741
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2026-02-12 22:29:26 -0600

    Update ReactApp.js

--- DietTracker/ReactApp.js
+++ DietTracker/ReactApp.js
@@ -4,8 +4,8 @@
 //import ReactDOM from "https://esm.sh/react-dom/client";
 //import React, { useState } from "https://esm.sh/react";
 
-import React, { useState } from "react";  
-import ReactDOM from "https://esm.sh/react-dom/client";
+import React, { useState } from "https://esm.sh/react@18.2.0";
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
 import "./ReactAppCSS.css";
 
 /*
