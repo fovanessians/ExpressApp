81ad910111d95954b94cf96b5455131bc2e71847
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2026-02-14 23:43:22 -0600

    Update DietTrackExpress.js

--- DietTracker/DietTrackExpress.js
+++ DietTracker/DietTrackExpress.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
+const app = express()
+const cors = require('cors')
 const mongoose = require("mongoose");
 const mySecret = process.env['MONGO_URI']
-mongoose.connect(mySecret, { dbName: 'Person' }, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mySecret, { dbName: 'DietExercise' }, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
 // User model***************************
-const User = mongoose.model('names',{
+const UserName = mongoose.model('names',{
     name: { type: String },
     age: { type: Number }
 });
