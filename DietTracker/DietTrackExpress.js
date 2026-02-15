38c398bbedfc48ba180f6725ee4629d2bb12d8c6
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2026-02-14 22:55:38 -0600

    Update DietTrackExpress.js

--- DietTracker/DietTrackExpress.js
+++ DietTracker/DietTrackExpress.js
@@ -1,1 +1,12 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const mySecret = process.env['MONGO_URI']
+mongoose.connect(mySecret, { dbName: 'Person' }, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+
+// User model***************************
+const User = mongoose.model('names',{
+    name: { type: String },
+    age: { type: Number }
+});
