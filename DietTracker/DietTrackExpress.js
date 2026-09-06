3eccacecfda76a9864558a473b57fefb3320081d
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2026-09-06 18:42:44 -0500

    Update DietTrackExpress.js

--- DietTracker/DietTrackExpress.js
+++ DietTracker/DietTrackExpress.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
+const router = express.Router();
 // ✅ Middleware (must come before routes)
 app.use(cors());
 app.use(express.json());
