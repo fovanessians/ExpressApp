d75bf2a215df977bc63bd52158755012758fe151
Frank Ovanessians <63524582+fovanessians@users.noreply.github.com>
2026-09-07 14:59:50 -0500

    Update DietTrackExpress.js

--- DietTracker/DietTrackExpress.js
+++ DietTracker/DietTrackExpress.js
@@ -89,12 +89,14 @@ router.get('/entry', async (req, res) => {
 });
 //**************************************
 //*****Routes for Calories****
-router.post('/:meal', (req, res) => {
+/*
+  router.post('/:meal', (req, res) => {
   const { meal } = req.params;
   const { calories } = req.body;
   console.log(meal, calories);
   res.json({ message: `${meal} saved` });
 });
+*/
 //*****Routes for Calories*****
 
 
